ca43c1f9b267fa2c3fae22b99b361e38a7e8a1f4
bywarmx <162827242+bywarmx@users.noreply.github.com>
2024-08-04 07:31:09 -0600

    Update hi.php

--- php/hi.php
+++ php/hi.php
@@ -1,3 +1,8 @@
+<?php
+	    header("Cache-Control: no-cache, must-revalidate");
+     header("Expires: Sat, 26 Jul 1997 05:00:00 GMT");
+     header("Pragma: no-cache");
+?>
 <html>
  <head>
   <title>Prueba de PHP</title>
